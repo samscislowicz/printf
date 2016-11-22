3fbaea58f69c69039b9042d2f88562c987646c6e
mroa5 <michael.roa@holbertonschool.com>
2016-11-22 18:24:10 +0000

    update

--- switch_func.c
+++ switch_func.c
@@ -1,7 +1,46 @@
 #include "holberton.h"
 
 /**
- * switchf - determine which specifier to use
+ * string_app - append two strs
+ * @base: initial string
+ * @str: string to add
+ * @location: position in format (i.e format[location])
+ *
+ * Return: pointer to appended base
+ */
+int string_app(char *base, char *str, int location)
+{
+	if (str == NULL)
+	{
+		_strcat(base, "(null)");
+		return (location + 1);
+	}
+	else
+	{
+		_strcat(base, str);
+		return (location + 1);
+	}
+}
+
+/**
+ * num_app - append string with converted number
+ * @base: initial string
+ * @num: number to be converted and added
+ * @location: location in format (ex. format[location])
+ *
+ * Return: pointer to appended base
+ */
+int num_app(char *base, int num, int location)
+{
+	char temp[100];
+
+	itoa(num, temp);
+	string_app(base, temp, 0);
+	return (location + 1);
+}
+
+/**
+ * switchf - determine which format to use
  * @format: format string from _printf
  * @buffer: buffer string from _printf
  * @i: location in @format
@@ -49,44 +88,4 @@ int switchf(const char *format, char *buffer, int i, int n, va_list args, int *s
 		break;
 	}
 	return (i);
-}
-
-/**
- * string_app - append two strings
- * @base: initial string
- * @str: string to add
- * @location: position in format (i.e format[location])
- *
- * Return: pointer to appended base
- */
-int string_app(char *base, char *str, int location)
-{
-	if (str == NULL)
-	{
-		_strcat(base, "(null)");
-		return (location + 1);
-	}
-	else
-	{
-		_strcat(base, str);
-		return (location + 1);
-	}
-}
-
-/**
- * num_app - append string with converted number
- * @base: initial string
- * @num: number to be converted and added
- * @location: location in format (i.e. format[location])
- *
- * Return: pointer to appended base
- */
-int num_app(char *base, int num, int location)
-{
-	char temp[100];
-
-	/* convert number to string */
-	itoa(num, temp);
-	string_app(base, temp, 0);
-	return (location + 1);
 }
